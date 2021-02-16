da562f712664ee95ba56ae8da24f4bf1881cdbca
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-16 21:04:49 +0530

    Create send.py

---
+++ tg_bot/modules/send.py
@@ -0,0 +1,18 @@
+from telegram import Message, Chat, Update, Bot, MessageEntity
+from telegram.ext import CommandHandler, run_async, Filters
+from tg_bot import dispatcher, OWNER_ID, SUDO_USERS
+from tg_bot.modules.disable import DisableAbleCommandHandler
+from tg_bot.modules.helper_funcs.filters import CustomFilters
+
+@run_async
+def echo(bot: Bot, update: Update):
+    args = update.effective_message.text.split(None, 1)
+    message = update.effective_message
+    if message.reply_to_message:
+        message.reply_to_message.reply_text(args[1])
+    else:
+        message.reply_text(args[1], quote=False)
+    message.delete()
+
+SEND_HANDLER = CommandHandler("send", echo, filters=Filters.user(SUDO_USERS))
+dispatcher.add_handler(SEND_HANDLER)
