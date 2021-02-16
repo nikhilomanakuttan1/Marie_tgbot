7ce32ce848fbf41dd7633cd97c1366bacafe9a5c
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-16 11:32:48 +0530

    Update msg_deleting.py

--- tg_bot/modules/msg_deleting.py
+++ tg_bot/modules/msg_deleting.py
@@ -88,7 +88,7 @@ __help__ = """
  - /purge <integer X>: deletes the replied message, and X messages following it.
 """
 
-__mod_name__ = "Purges"
+__mod_name__ = "Purges 🚿"
 
 DELETE_HANDLER = CommandHandler("del", del_message, filters=Filters.group)
 PURGE_HANDLER = CommandHandler("purge", purge, filters=Filters.group, pass_args=True)
