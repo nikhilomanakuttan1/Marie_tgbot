fcd19f70bc24cfd51ca98af8375150f8230a0dd0
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-16 13:02:16 +0530

    Update backups.py

--- tg_bot/modules/backups.py
+++ tg_bot/modules/backups.py
@@ -72,7 +72,7 @@ __help__ = """
 *Admin only:*
  - /import: reply to a group butler backup file to import as much as possible, making the transfer super simple! Note \
 that files/photos can't be imported due to telegram restrictions.
- - /export: !!! This isn't a command yet, but should be coming soon!
+ - /export: This isn't a command yet, but should be coming soon!
 """
 IMPORT_HANDLER = CommandHandler("import", import_data)
 EXPORT_HANDLER = CommandHandler("export", export_data)
