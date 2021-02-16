60059abd7ba19ffa40f7c06eba98bf4b01e9da7a
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-16 10:21:32 +0530

    Update tts.py

--- tg_bot/modules/tts.py
+++ tg_bot/modules/tts.py
@@ -1,16 +1,15 @@
 from datetime import datetime
 
 from gtts import gTTS
-from telegram import ChatAction, Update
-from telegram.ext import CallbackContext, run_async, CommandHandler, MessageHandler, Filters
+from telegram import ChatAction, Update, Message
+from telegram.ext import run_async, CommandHandler, MessageHandler, Filters
 
 from tg_bot import dispatcher
 from tg_bot.modules.disable import DisableAbleCommandHandler
 
 
 @run_async
-def tts(update: Update, context: CallbackContext):
-    args = context.args
+def tts(bot: Bot, update: Update):
     current_time = datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")
     datetime.now().strftime("%d%m%y-%H%M%S%f")
     reply = " ".join(args)
