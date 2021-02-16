63977a7387217bc0eca6c005b4b3cdc8081de627
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2021-02-16 11:23:54 +0530

    Update bans.py

--- tg_bot/modules/bans.py
+++ tg_bot/modules/bans.py
@@ -449,7 +449,7 @@ __help__ = """
  - /kick <userhandle>: kicks a user, (via handle, or reply)
 """
 
-__mod_name__ = "Bans 🛑"
+__mod_name__ = "Bans ❌"
 
 BAN_HANDLER = CommandHandler("ban", ban, pass_args=True, filters=Filters.group)
 TEMPBAN_HANDLER = CommandHandler(["tban", "tempban"], temp_ban, pass_args=True, filters=Filters.group)
